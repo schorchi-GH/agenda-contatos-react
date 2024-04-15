0566a36a88eddac334b4563b94c0366b2d2159c3
Schorchi <schorchi@gmail.com>
2024-04-15 19:17:05 -0300

    exercicio cypress alterar, criar e remover contato agenda

--- cypress/integration/inclusao/incluir_contato.spec.js
+++ cypress/integration/inclusao/incluir_contato.spec.js
@@ -4,15 +4,12 @@ describe('Testes de Inclusão de Contato', () => {
     });
   
     it('deve permitir a inclusão de um novo contato', () => {
-      // Preencha os campos do formulário. Use seletores baseados em atributos como nome ou placeholder, se disponíveis.
       cy.get('input').first().type('Ótimo Exercício');
       cy.get('input[type="email"]').type('media@100porcento.com');
       cy.get('input[type="tel"]').type('11989118110');
   
-      // Envie o formulário de adição de contato
       cy.get('button[class="adicionar"]').click();
   
-      // Verifique se o novo contato foi adicionado. Esta é uma asserção simples.
       cy.contains('Ótimo Exercício').should('be.visible');
       cy.contains('media@100porcento.com').should('be.visible');
       cy.contains('11989118110').should('be.visible');
--- cypress/integration/remocao/remover_contato.spec.js
+++ cypress/integration/remocao/remover_contato.spec.js
@@ -4,10 +4,9 @@ describe('Testes de Remoção de Contato', () => {
     });
   
     it('deve permitir a remoção de um contato', () => {
-      // Encontrar e clicar no botão de exclusão para qualquer contato
       cy.get('button.delete').first().click();
   
-      // Verificar se o contato foi removido com sucesso
+
       cy.contains('Felice Lacerda').should('not.exist');
     });
   });
